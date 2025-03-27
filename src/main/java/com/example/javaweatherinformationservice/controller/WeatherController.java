40b46325f79fbd793d6ba8cc1a9209f8d16d8be6
Colton Randall <coltonrandall.nz@gmail.com>
2025-03-28 10:25:43 +1300

    add base structure and methods to service and controller

--- src/main/java/com/example/javaweatherinformationservice/controller/WeatherController.java
+++ src/main/java/com/example/javaweatherinformationservice/controller/WeatherController.java
@@ -1,4 +1,36 @@
 package com.example.javaweatherinformationservice.controller;
 
+import com.example.javaweatherinformationservice.model.Weather;
+import com.example.javaweatherinformationservice.service.WeatherService;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+// Using Spring Boots built in support for REST APIs
+// Expose data via API endpoints
+@RestController
+@RequestMapping("/weather")
 public class WeatherController {
+
+    private WeatherService weatherService;
+
+    public WeatherController(WeatherService weatherService) {
+        this.weatherService = weatherService;
+    }
+
+    // Simple GET methods for city & all weather
+    public void getCityWeather(@PathVariable String cityName) {
+        // TODO logic to come
+        System.out.println("City Weather information from API");
+        // TODO uncomment below code when service has logic
+//        return weatherService.getCityWeatherInformation(cityName);
+    }
+
+    public void getAllWeather(){
+        System.out.println("All Weather information from API");
+        // TODO uncomment below code when service has logic
+//        return weatherService.getAllWeatherInformation();
+    }
+
+
 }
--- src/main/java/com/example/javaweatherinformationservice/service/WeatherService.java
+++ src/main/java/com/example/javaweatherinformationservice/service/WeatherService.java
@@ -1,4 +1,26 @@
 package com.example.javaweatherinformationservice.service;
 
+import org.springframework.stereotype.Service;
+
+// Gets data from in-memory storage, or fetches from API
+@Service
 public class WeatherService {
+
+
+
+    // base method for retrieving weather based on city name
+    public void getCityWeatherInformation(String cityName) {
+        // TODO logic to come
+        System.out.println(cityName);
+    }
+
+    public void getAllWeatherInformation() {
+        // TODO logic to come
+        System.out.println("Weather information");
+    }
+
+    public void fetchWeatherFromMockApi(){
+        // TODO logic to come
+        System.out.println("Weather information from API");
+    }
 }
