d2b4f331078ce2ef472d17a06a96b706a7108abc
Colton Randall <coltonrandall.nz@gmail.com>
2025-03-28 11:26:53 +1300

    add simple UI for user to enter city name

--- src/main/java/com/example/javaweatherinformationservice/client/WeatherClient.java
+++ src/main/java/com/example/javaweatherinformationservice/client/WeatherClient.java
@@ -1,12 +1,25 @@
 package com.example.javaweatherinformationservice.client;
 
+import com.example.javaweatherinformationservice.model.Weather;
+import com.example.javaweatherinformationservice.service.WeatherData;
+import com.example.javaweatherinformationservice.service.WeatherService;
+
+import java.util.Optional;
+import java.util.Scanner;
+
 /*
     Mock the user to print out to the console
  */
 public class WeatherClient {
 
+    WeatherData weatherData = new WeatherData();
+    WeatherService weatherService = new WeatherService(weatherData); // Should only create this once for reuse.
 
     public void run(){
-        System.out.println("Running Weather Client");
+        System.out.println("Enter City Name: ");
+        Scanner scanner = new Scanner(System.in);
+        String city = scanner.nextLine();
+        Optional<Weather> weather = weatherService.getCityWeatherInformation(city);
+        System.out.println(weather); // have to convert this output to JSON
     }
 }
