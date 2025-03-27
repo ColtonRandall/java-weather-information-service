5a996ec515569ec8197f682f9aee0c376c8730b5
Colton Randall <coltonrandall.nz@gmail.com>
2025-03-28 10:02:55 +1300

    initialise java spring boot application

---
+++ src/main/java/com/example/javaweatherinformationservice/JavaWeatherInformationServiceApplication.java
@@ -0,0 +1,14 @@
+package com.example.javaweatherinformationservice;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class JavaWeatherInformationServiceApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(JavaWeatherInformationServiceApplication.class, args);
+
+    }
+
+}
---
+++ src/test/java/com/example/javaweatherinformationservice/JavaWeatherInformationServiceApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.javaweatherinformationservice;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class JavaWeatherInformationServiceApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
