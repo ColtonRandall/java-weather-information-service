0857102f0ff7ef4e7526eb14fe16480e5b00933e
Colton Randall <coltonrandall.nz@gmail.com>
2025-03-28 11:38:00 +1300

    add logic for viewing weather for one city/all cities

--- src/main/java/com/example/javaweatherinformationservice/client/WeatherClient.java
+++ src/main/java/com/example/javaweatherinformationservice/client/WeatherClient.java
@@ -6,6 +6,7 @@ import com.example.javaweatherinformationservice.service.WeatherService;
 import com.fasterxml.jackson.core.JsonProcessingException;
 import com.fasterxml.jackson.databind.ObjectMapper;
 
+import java.util.Map;
 import java.util.Optional;
 import java.util.Scanner;
 
@@ -17,21 +18,35 @@ public class WeatherClient {
     WeatherData weatherData = new WeatherData();
     WeatherService weatherService = new WeatherService(weatherData); // Should only create this once for reuse.
     ObjectMapper objectMapper = new ObjectMapper();
+    Scanner scanner = new Scanner(System.in);
+    String json;
 
     public void run() throws Exception {
-        System.out.println("Enter City Name: ");
-        Scanner scanner = new Scanner(System.in);
-        String city = scanner.nextLine();
-        Optional<Weather> weather = weatherService.getCityWeatherInformation(city);
+        System.out.println("Enter city name: \n or press 'a' to view all cities");
+        String response = scanner.nextLine();
+
+        if (response.equals("a")) {
+            Map<String, Weather> weather = weatherService.getAllWeatherInformation();
+            weather.forEach((city, weatherData) -> {
+                try{
+                    json = objectMapper.writerWithDefaultPrettyPrinter().writeValueAsString(weatherData);
+                    System.out.println(json);
+                } catch (JsonProcessingException e) {
+                    throw new RuntimeException(e);
+                }
+            });
+        }
+
+        Optional<Weather> weather = weatherService.getCityWeatherInformation(response);
         try{
             if(weather.isPresent()){
-                String json = objectMapper.writerWithDefaultPrettyPrinter().writeValueAsString(weather.get());
+                json = objectMapper.writerWithDefaultPrettyPrinter().writeValueAsString(weather.get());
                 System.out.println(json);
             } else {
-                System.out.println(city + " not found");
+                System.out.println(response + " not found");
             }
         } catch (JsonProcessingException e) {
-            throw new Exception("Json processing error for " + city);
+            throw new Exception("Json processing error for " + response);
         }
     }
 }
